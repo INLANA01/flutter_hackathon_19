d1712b8b1b43cc2d6f41a5012ba1cb91be3c0872
Aditya Avinash Landge <al2960@ad.rit.edu>
2019-06-01 17:15:06 -0400

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Quote of the Day'),
+          title: Text('English to Marathi Translation'),
         ),
         body: Center(
           child: FutureBuilder<Quote>(
@@ -51,28 +51,39 @@ class MyApp extends StatelessWidget {
 
 
   Future<Quote> getQuote() async {
-    String url = 'https://quotes.rest/qod.json';
-    final response =
-        await http.get(url, headers: {"Accept": "application/json"});
+    // String url = 'https://quotes.rest/qod.json';
+    String url2 = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190601T192331Z.b0e37cd519d41081.1e127d3c8195045f3489ea3c204579cbb12b0349&text=%3Chand%3E&lang=mr';
+    // final response =
+    //     await http.get(url, headers: {"Accept": "application/json"});
+    var resp = await http.get(url2, headers: {"Accept": "application/json"});
 
-
-    if (response.statusCode == 200) {
-      return Quote.fromJson(json.decode(response.body));
+    if (resp.statusCode == 200) {
+      return Quote.fromJson(json.decode(resp.body));
     } else {
       throw Exception('Failed to load post');
     }
+
+    // if (response.statusCode == 200) {
+    //   return Quote.fromJson(json.decode(response.body));
+    // } else {
+    //   throw Exception('Failed to load post');
+    // }
   }
 }
 
 class Quote {
+  // final var authoe
   final String author;
   final String quote;
 
   Quote({this.author, this.quote});
 
   factory Quote.fromJson(Map<String, dynamic> json) {
-    return Quote(
-        author: json['contents']['quotes'][0]['author'],
-        quote: json['contents']['quotes'][0]['quote']);
+        return Quote(
+        author: json['text'][0].toString(),
+    // return Quote(
+    //     author: json['contents']['quotes'][0]['author'],
+        // quote: json['contents']['quotes'][0]['quote']);
+        quote: json['lang']);
   }
 }
